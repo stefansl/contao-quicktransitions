b574cbffe703c1f5d30720233aa68a00a962c9c8
stefansl <ssl@clickpress.de>
2019-07-15 19:12:59 +0200

    Reformat

--- src/Resources/public/quicktransitions.js
+++ src/Resources/public/quicktransitions.js
@@ -1,44 +1,45 @@
 /*
  * Contao Quick Transitions
+ *
  * @author: Stefan Schulz-Lauterbach <ssl@clickpress.de>
  */
 
+(function() {
+  'use strict'
+  if (window.innerWidth >= 768)
+  {
+    var intFrameHeight = window.innerHeight
 
+    // Animations
+    var elAnimation = document.querySelectorAll('[data-animation]'),
+        countContainer = elAnimation.length
 
-(function () {
-  'use strict';
-  if (window.innerWidth >= 768) {
-    var intFrameHeight = window.innerHeight;
+    var animateInViewport = function() {
+      for (var i = 0; i < countContainer; ++i)
+      {
+        var pos = elAnimation[i].getBoundingClientRect(),
+            rounded = Math.round(pos.top) - 100
 
-  // Animations
-  var elAnimation = document.querySelectorAll('[data-animation]'),
-      countContainer = elAnimation.length;
-
-  var animateInViewport = function () {
-    for (var i = 0; i < countContainer; ++i) {
-      var pos = elAnimation[i].getBoundingClientRect(),
-        rounded = Math.round(pos.top) - 100;
-
-      if (rounded <= intFrameHeight) {
-        elAnimation[i].classList.add('animate');
+        if (rounded <= intFrameHeight)
+        {
+          elAnimation[i].classList.add('animate')
+        }
       }
     }
-  };
 
-    var startAnimation = function () {
-      animateInViewport();
-    };
+    var startAnimation = function() {
+      animateInViewport()
+    }
 
     // Initial fade in anim on load
-    animateInViewport();
+    animateInViewport()
   }
 
-
-  window.addEventListener('scroll', function () {
-    requestAnimationFrame(startAnimation);
+  window.addEventListener('scroll', function() {
+    requestAnimationFrame(startAnimation)
   }, {
     capture: true,
     passive: true
-  });
+  })
 
-}());
+}())

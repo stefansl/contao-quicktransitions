73d283a325c2dc6608fc8af6016119040dce34df
stefansl <ssl@clickpress.de>
2022-09-01 15:28:54 +0200

    Refactoring

--- src/DependencyInjection/ContaoQuicktransitionsExtension.php
+++ src/DependencyInjection/ContaoQuicktransitionsExtension.php
@@ -11,6 +11,6 @@ class ContaoQuicktransitionsExtension extends Extension
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yaml');
+        $loader->load('listener.yaml');
     }
 }

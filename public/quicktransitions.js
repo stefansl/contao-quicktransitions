55815e84b116a657a33f8465f3631d9d40567d81
stefansl <ssl@clickpress.de>
2024-10-18 17:50:29 +0200

    Add resize event listener and improve code readability
    Added a resize event listener to trigger animations on window size change. Improved code readability by using "const" for variables and better indentation.

--- public/quicktransitions.js
+++ public/quicktransitions.js
@@ -5,41 +5,47 @@
  */
 
 (function() {
-  'use strict'
-  if (window.innerWidth >= 768)
-  {
-    var intFrameHeight = window.innerHeight
+    'use strict'
+    if (window.innerWidth >= 768)
+    {
+        var intFrameHeight = window.innerHeight
 
-    // Animations
-    var elAnimation = document.querySelectorAll('[data-animation]'),
-        countContainer = elAnimation.length
+        // Animations
+        var elAnimation = document.querySelectorAll('[data-animation]'),
+            countContainer = elAnimation.length
 
-    var animateInViewport = function() {
-      for (var i = 0; i < countContainer; ++i)
-      {
-        var pos = elAnimation[i].getBoundingClientRect(),
-            rounded = Math.round(pos.top) - 100
+        var animateInViewport = function() {
+            for (var i = 0; i < countContainer; ++i) {
+                const pos = elAnimation[i].getBoundingClientRect(),
+                    rounded = Math.round(pos.top) - 100
 
-        if (rounded <= intFrameHeight)
-        {
-          elAnimation[i].classList.add('animate')
+                if (rounded <= intFrameHeight) {
+                    elAnimation[i].classList.add("animate")
+                }
+            }
         }
-      }
+
+        var startAnimation = function() {
+            animateInViewport()
+        }
+
+        // Initial fade in anim on load
+        animateInViewport()
     }
 
-    var startAnimation = function() {
-      animateInViewport()
-    }
+    window.addEventListener("resize", function() {
+        requestAnimationFrame(startAnimation)
+    }, {
+        capture: true,
+        passive: true
+    })
 
-    // Initial fade in anim on load
-    animateInViewport()
-  }
-
-  window.addEventListener('scroll', function() {
-    requestAnimationFrame(startAnimation)
-  }, {
-    capture: true,
-    passive: true
-  })
+    window.addEventListener("scroll", function() {
+        requestAnimationFrame(startAnimation)
+    }, {
+        capture: true,
+        passive: true
+    })
 
 }())
+
